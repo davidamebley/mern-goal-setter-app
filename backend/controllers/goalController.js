0e9668a0fdb83417192e9eaddb528a4b37804df7
David Amebley <davameb@gmail.com>
2022-11-14 17:59:29 +0200

    Create CRUD functionality for our API

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -32,14 +32,33 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({'message': `Update Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete a goal
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({'message': `Delete Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()     // Delete item
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
