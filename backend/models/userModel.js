65aa276c78e31af6ac53e8af06e54952abebba67
David Amebley <davameb@gmail.com>
2022-11-15 02:12:05 +0200

    Create User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name field']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email field'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password field']
+    },
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema)
