1f296a461d3674362960610aa9154e3428c739b3
David Amebley <davameb@gmail.com>
2022-11-16 04:02:00 +0200

    Prevent unauthorized users fom perform update and delete

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
-const protect = require('../middleware/authMiddleware')
 
-// Calling our routes
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+const { protect } = require('../middleware/authMiddleware')
+
+// Calling our routes. Putting 'protect' before a func. name protects that route
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
